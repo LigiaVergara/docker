d85e52ffeefc2d764f9832c60476cb7e0aed91f5
LigiaVergara <147092166+LigiaVergara@users.noreply.github.com>
2024-05-06 23:13:18 +0200

    Adding html code

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ import redis
 from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
+import pandas as pd
+import json
 
 load_dotenv() 
 cache = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379,  password=os.getenv('REDIS_PASSWORD'))
@@ -23,6 +25,25 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return render_template('hello.html', name= "BIPM", count = count)
+
+@app.route('/Titanic')
+def titanic():
+    df = pd.read_csv("titanic.csv")
+    # Calculate number of male and female survivors
+    survived_by_sex = df[df['survived'] == 1]['sex'].value_counts()
+    # Convert NumPy integer values to Python integers
+    bar_labels = ['Male', 'Female']
+    bar_data = [int(survived_by_sex.get('male', 0)), int(survived_by_sex.get('female', 0))]
+
+    # Render HTML table
+    html_table = df.head().to_html()
+
+    # Render bar chart data
+    bar_labels_json = json.dumps(bar_labels)
+    bar_data_json = json.dumps(bar_data)
+
+    return render_template('titanic.html', table=html_table, bar_labels=bar_labels, bar_data=bar_data)
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
